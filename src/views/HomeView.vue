f0ab42061e11640a4be0787fc55318d1eeb21682
pheaktrayith <yithsopheaktra18@email.com>
2023-05-07 11:05:30 +0700

    first commit

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,36 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="w-3/4 mx-auto p-5 mt-10 shadow-lg">
+    <!-- logo -->
+    <div>
+      <img id="logo" src="@/assets/nature-logo.png">
+    </div>
+    <!-- img -->
+    <div>
+      <img class="rounded-lg w-full" id="img-1" src="@/assets/vector-palm-trees-against-sunset-sky.jpg">
+    </div>
+    <div>
+      <h1 class="text-green-700 font-semibold font-mono text-lg">Sunset Destination</h1>
+      <p class="text-gray-700 tracking-wide leading-6 font-serif">Lorem, ipsum dolor sit amet consectetur adipisicing
+        elit. Iste, consectetur earum! Laboriosam id cum quisquam facere magni tempora, explicabo porro quidem placeat
+        voluptatibus illo a aut maiores aliquam at aspernatur libero et impedit eligendi quos. Neque quos tempore
+        architecto consequuntur, nisi amet culpa ea veniam nam voluptates aspernatur, quaerat quidem!</p>
+      <button class="shadow-xl transform transition-all duration-150 hover:translate-y-1 hover:text-yellow-300 text-yellow-400 bg-green-700 font-mono text-lg rounded-lg px-16 py-2 mt-5">
+          Visit
+      </button>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
+
 }
 </script>
+
+<style>
+#logo {
+  width: 180px;
+}
+
+
+</style>
